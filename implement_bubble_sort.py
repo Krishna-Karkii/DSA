2f5fa28774962efe6179481a2f4f88e4c1a47ce0
krishna <krishnakarki1996@gmail.com>
2024-08-14 15:07:20 +0545

    swap values, sort random list of user defined length

--- implement_bubble_sort.py
+++ implement_bubble_sort.py
@@ -1,3 +1,13 @@
+import random
+
+
+def swap_values(j, i, data):
+    """swap the values of two indexes"""
+    temp = data[i]
+    data[i] = data[j]
+    data[j] = temp
+    return data
+
 
 def bubble_sort(data, length):
     """implement bubble sort algorithm."""
@@ -7,4 +17,17 @@ def bubble_sort(data, length):
                 if data[j] > data[i]:
                     data = swap_values(j, i, data)
 
+    return data
 
+
+# Get the length of list from user, fill with random number
+length = int(input("Enter the size of random list: "))
+data = []
+for i in range(length):
+    data.append(random.randint(1, 50))
+
+print("Before Sorting: ", data)
+
+# sort the data using bubble sort and print it
+sorted_data = bubble_sort(data, length)
+print("After Sorting: ", sorted_data)
