31de52df8ce9fa7fd5a55846fe3198520b2d0d7f
krishna <krishnakarki1996@gmail.com>
2024-08-14 17:43:55 +0545

    made code efficient

--- implement_bubble_sort.py
+++ implement_bubble_sort.py
@@ -13,9 +13,8 @@ def bubble_sort(data, length):
     """implement bubble sort algorithm."""
     for i in range(length - 1, -1, -1):
         for j in range(length):
-            if j < i:
-                if data[j] > data[i]:
-                    data = swap_values(j, i, data)
+            if j < i and data[j] > data[i]:
+                data = swap_values(j, i, data)
 
     return data
 
