c9513b07eb32670ba77313ba0873ce3647575c00
krishna <krishnakarki1996@gmail.com>
2024-08-14 17:44:20 +0545

    made code efficient, asked for user input

--- implement_insertion_sort.py
+++ implement_insertion_sort.py
@@ -1,3 +1,6 @@
+import random
+
+
 def swap_values(i, j, data):
     """swap value of two indexes."""
     temp = data[i]
@@ -8,7 +11,7 @@ def swap_values(i, j, data):
 
 def insertion_sort(data, length):
     """implement the insertion sort algorithm."""
-    for i in range(length):
+    for i in range(1, length):
         j = i
         while j > 0 and data[j - 1] > data[j]:
             data = swap_values(j - 1, j, data)
@@ -17,5 +20,16 @@ def insertion_sort(data, length):
     return data
 
 
-sorted_data = insertion_sort([7, 5, 4, 6, 1, 2, 3], length=7)
-print(sorted_data)
+# asking the user for the length of random list
+length = int(input("Enter a length of random list: "))
+
+# creating a random list
+list_to_sort = []
+for i in range(length):
+    list_to_sort.append(random.randint(1, 100))
+
+print("Before Sorting: ", list_to_sort)
+
+# Sorting using insertion sort
+sorted_list = insertion_sort(list_to_sort, length)
+print("After Sorting: ", sorted_list)
